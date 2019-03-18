3e50dfb16bd27b4d49c35e71c61f572814e13699
effsoft <herilan@hotmail.com>
2019-03-18 21:52:18 +0800

    init

---
+++ src/JqueryAssetBundle.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace effsoft\eff\asset\jquery;
+
+use effsoft\eff\EffAssetBundle;
+
+class JqueryAssetBundle extends EffAssetBundle{
+
+    public $sourcePath = '@bower/jquery/dist';
+
+    public function init(){
+        parent::init();
+
+    }
+
+    public $js = [
+        YII_ENV_DEV ? 'jquery.js':'jquery.min.js',
+    ];
+
+    public $publishOptions = [
+        'only' => [
+            'js/jquery.js',
+            'js/jquery.min.js',
+        ],
+    ];
+
+}
